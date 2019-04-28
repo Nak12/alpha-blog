cd65a4b46e72b1d90df10063e1a606f180cd93cb
Kaio Masaru Nakazono <kaio.webtech@gmail.com>
2019-04-28 14:26:06 -0300

    create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+
   def new
     @user = User.new
   end
@@ -14,12 +16,10 @@ class UsersController < ApplicationController
     end 
   end
 
-  def edit
-    @user = User.find(params[:id])    
+  def edit 
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params) 
       flash[:success] = "Your account was updated successfully."
       redirect_to articles_path
@@ -28,8 +28,15 @@ class UsersController < ApplicationController
     end 
   end
 
+  def show
+  end
+
   private
     def user_params 
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
       when :alert then "alert alert-warning"
     end
   end
+
+  def gravatar_for(user, options = {size: 80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded-circle") #image_tag é um método do Rails
+  end 
 end
