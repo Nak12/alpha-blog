5cedbfb3106d6fd514facafa394fd0ec01e38a29
Kaio Masaru Nakazono <kaio.webtech@gmail.com>
2019-04-28 19:07:25 -0300

    complete authentication system and update current_user to be article creator

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def home
+    redirect_to articles_path if logged_in? #se já estiver logado vai para pagina dos artigos, não entra no root_path
   end 
 
   def about
